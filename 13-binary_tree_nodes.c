8f019f7292aa5174f45d85176857537440bab418
Adepoju Lateef <badepojulateef@gmail.com>
2023-03-03 02:58:21 +0100

    attempted task 12

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Count the leaves in a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to count the number of leaves
+ *
+ * Return: Always 0 (Success)
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	return (1 + binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
