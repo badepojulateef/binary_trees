c803aac21de44a5bb33a7f51db9dadae9c507284
Adepoju Lateef <badepojulateef@gmail.com>
2023-03-03 03:57:14 +0100

    divided line 46 in to 2 in task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -43,7 +43,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		if (tree->left == NULL || tree->right == NULL)
 			return (1);
 		else
-			return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+			return (
+					binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right)
+					);
 	}
 	else
 		return (0);
