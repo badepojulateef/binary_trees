713b1e700d74e7fc1b64b9afc2f48ed3e198b655
Adepoju Lateef <badepojulateef@gmail.com>
2023-03-03 02:30:37 +0100

    attempted task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -3,27 +3,40 @@
 #include "binary_trees.h"
 
 /**
- * main - Entry point
+ * binary_tree_leaves - Count the leaves in a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to count the number of leaves
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	binary_tree_t *root;
-	size_t leaves;
+	if (tree == NULL)
+		return (0);
 
-	root = binary_tree_node(NULL, 98);
-	root->left = binary_tree_node(root, 12);
-	root->right = binary_tree_node(root, 402);
-	binary_tree_insert_right(root->left, 54);
-	binary_tree_insert_right(root, 128);
-	binary_tree_print(root);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-	leaves = binary_tree_leaves(root);
-	printf("Leaves in %d: %lu\n", root->n, leaves);
-	leaves = binary_tree_leaves(root->right);
-	printf("Leaves in %d: %lu\n", root->right->n, leaves);
-	leaves = binary_tree_leaves(root->left->right);
-	printf("Leaves in %d: %lu\n", root->left->right->n, leaves);
-	return (0);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
