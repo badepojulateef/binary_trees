d633c4cb59a06031f758d1b8fc5c4440c224aa15
Adepoju Lateef <badepojulateef@gmail.com>
2023-03-04 05:21:30 +0100

    corrected task 26

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -3,17 +3,20 @@
 /**
  * array_to_bst - builds a Binary Search Tree from an array
  * @array: pointer to the first element of the array to be converted
- * @size: the number of elements in the array
- * Return: A pointer to the root node of the created BST
- *         NULL on failure
+ * @size: number of elements in the array
+ *
+ * Return: pointer to the root node of the created BST, or NULL on failure
  */
 bst_t *array_to_bst(int *array, size_t size)
 {
-	unsigned int i;
 	bst_t *root = NULL;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
-		bst_insert(&root, array[i]);
+	bst_insert(&root, array[i]);
 
 	return (root);
 }
